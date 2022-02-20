60887cf0e6d99edfc5ff2b6beaa44479dedc5248
plyta <sevenieq@gmail.com>
2022-02-21 00:17:55 +0100

    Bace schema

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,12 @@
 selectorR = document.getElementsByClassName("selectionType");
-      for(el of selectorR){
-        el.addEventListener("click",(e)=>{
-            console.log(e.target.innerHTML );
-        })
-      }
+for (el of selectorR) {
+  el.addEventListener("click", (e) => {
+    console.log(e.target.innerHTML);
+  });
+}
+
+listOfItems = document.getElementsByClassName("itemDs");
+for (el of listOfItems) {
+    el.setAttribute('style', `background-color:rgba(0, ${el.dataset.numvalue}, 0, 1)`);
+    console.log(el.dataset.numvalue)
+}
