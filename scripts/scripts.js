17a7dca1697a9536de79cc1fa7b87af01f1976a1
plyta <sevenieq@gmail.com>
2022-02-22 00:52:31 +0100

    Finished

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,10 @@
 selectorR = document.getElementsByClassName("selectionType");
 containerOfItems = document.getElementById("display");
-amountRange = document.getElementById('amountrange');
-speedSort = document.getElementById('speedrange').valueAsNumber;
+amountRange = document.getElementById("amountrange");
+sortButton = document.getElementById("sortButton");
+resetButton = document.getElementById("resetButton");
+logg = document.getElementById("logg");
+speedSort = document.getElementById("speedrange").valueAsNumber;
 
 for (el of selectorR) {
   el.addEventListener("click", (e) => {
@@ -16,15 +19,16 @@ for (el of selectorR) {
 
 function generateArrayItems(num) {
   containerOfItems.innerHTML = "";
-  for (let x=0; x < num; x++) {
+  for (let x = 0; x < num; x++) {
     const ran = randomValueNum();
     let div = document.createElement("div");
-    div.innerHTML = `${ran}`
-    div.className = `itemDs`
-    div.style.backgroundColor = `rgba( ${ran}, ${ran}, ${ran} , 1)`
-    div.style.height = `${18 + (ran * 0.4)}%`
-    div.style.fontSize = `${250 / (num/2)}px`
-    div.dataset.numvalue =`${ran}`
+    div.innerHTML = `${ran}`;
+    div.attributes.alt = "TEXT";
+    div.className = `itemDs`;
+    div.style.backgroundColor = `rgba( ${ran}, ${ran}, ${ran} , 1)`;
+    div.style.height = `${18 + ran * 0.4}%`;
+    div.style.fontSize = `${250 / (num / 2)}px`;
+    div.dataset.numvalue = `${ran}`;
     containerOfItems.appendChild(div);
   }
 }
@@ -34,42 +38,57 @@ function randomValueNum() {
   const max = Math.floor(200);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+function ressetAray(){
+  logg.innerHTML = 'logic: <span class="lTrue">value</span>'
+  document.getElementById("amountrange").value  = 20
+  document.getElementById("speedrange").value  = 500
+  speedSort = 500;
+  generateArrayItems(20);
+}
 generateArrayItems(20);
 
-
-amountRange.addEventListener("change", function(e) {
+amountRange.addEventListener("change", function (e) {
   generateArrayItems(e.target.valueAsNumber);
 });
+document.getElementById("speedrange").addEventListener("change", function (e) {
+  speedSort = document.getElementById("speedrange").valueAsNumber;
+});
 
+function delay(n){
+  return new Promise(function(resolve){
+      setTimeout(resolve,n);
+  });
+}
 
-
-function bblSort(){
+async  function bblSort(){
+  sortButton.disabled  = true
+  amountRange.disabled  = true
+  resetButton.disabled  = true
   arr = document.getElementsByClassName("itemDs")
-  let i = 0;
-  setInterval(function(){
-    if(i < arr.length){
-      for(let j = 0; j < ( arr.length - i -1 ); j++){
-        if(parseFloat(arr[j].dataset.numvalue) >parseFloat( arr[j+1].dataset.numvalue)){
-          let temp = arr[j].outerHTML
-          arr[j].outerHTML = arr[j + 1].outerHTML
-          arr[j+1].outerHTML = temp
-        }
+  for(var i = 0; i < arr.length; i++){
+    for(var j = 0; j < ( arr.length - i -1 ); j++){
+      if(parseFloat(arr[j].dataset.numvalue) > parseFloat(arr[j+1].dataset.numvalue)){
+        logg.innerHTML = `${parseFloat(arr[j].dataset.numvalue)} > ${parseFloat(arr[j+1].dataset.numvalue)}: <span class="lTrue">TRUE - swap</span>`
+      }else{
+        logg.innerHTML = `${parseFloat(arr[j].dataset.numvalue)} > ${parseFloat(arr[j+1].dataset.numvalue)}: <span class="lFalse">FALSE - skip</span>`
       }
-      i++;
+      arr[j].classList.toggle("selected")
+      arr[j+1].classList.toggle("selected")
+      await delay(1010 - speedSort);
+
+      if(parseFloat(arr[j].dataset.numvalue) > parseFloat(arr[j+1].dataset.numvalue)){
+        let temp = arr[j].outerHTML
+        arr[j].outerHTML = arr[j + 1].outerHTML
+        arr[j+1].outerHTML = temp   
+      }
+      arr[j].classList.toggle("selected")
+      arr[j+1].classList.toggle("selected")
     }
-  },speedSort)
-  for(x of arr){
-    console.log(x.dataset.numvalue)
-  } 
- }
+  }
+  logg.innerHTML = 'Sorting: <span class="lTrue">complete</span>'
+  sortButton.disabled  = false
+  amountRange.disabled  = false
+  resetButton.disabled  = false
+}
  
-
-//  function switchPos(){
-//    console.log("x")
-//   arr = document.getElementsByClassName("itemDs")
-//         var temp = arr[0].outerHTML 
-//         arr[0].outerHTML  = arr[1].outerHTML 
-//         arr[1].outerHTML  = temp
-
-//   }
-// setInterval(switchPos,500)
