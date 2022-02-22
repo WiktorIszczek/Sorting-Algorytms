20dfb38c6cc05a7d046ada6b31a04410e61ebe42
plyta <sevenieq@gmail.com>
2022-02-22 02:49:32 +0100

    sorting inseration

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,11 +4,17 @@ amountRange = document.getElementById("amountrange");
 sortButton = document.getElementById("sortButton");
 resetButton = document.getElementById("resetButton");
 logg = document.getElementById("logg");
+headder = document.getElementById("sortingType");
 speedSort = document.getElementById("speedrange").valueAsNumber;
+let sortingMethod = "Bubble";
 
 for (el of selectorR) {
   el.addEventListener("click", (e) => {
-    console.log(e.target.innerHTML);
+    if(sortButton.disabled==false){
+      sortingMethod = e.target.innerHTML
+      headder.innerHTML = `Type: ${sortingMethod}`
+      console.log(sortingMethod);
+    }
   });
 }
 
@@ -61,6 +67,20 @@ function delay(n){
   });
 }
 
+function startSorting(){
+  switch (sortingMethod) {
+    case "Bubble":
+      bblSort()
+      break;
+  
+    case "Inseration":
+      insertionSort()
+      break;
+    default:
+      alert(ERROR)
+  }
+}
+
 async  function bblSort(){
   sortButton.disabled  = true
   amountRange.disabled  = true
@@ -90,5 +110,32 @@ async  function bblSort(){
   sortButton.disabled  = false
   amountRange.disabled  = false
   resetButton.disabled  = false
+  headder.innerHTML = `Type: ${sortingMethod}`
 }
- 
+
+async function insertionSort() {
+  sortButton.disabled  = true
+  amountRange.disabled  = true
+  resetButton.disabled  = true
+  arr = document.getElementsByClassName("itemDs")
+  let n = arr.length;
+      for (let i = 1; i < n; i++) {
+          let current = arr[i];
+          current.classList.toggle("pointer")
+          logg.innerHTML = `current: <span class="lTrue">${current.dataset.numvalue}</span>`
+          let j = i-1; 
+          while ((j > -1) && ((parseFloat(current.dataset.numvalue)) < (parseFloat(arr[j].dataset.numvalue)))) {
+            await delay(1010 - speedSort);
+            arr[j].classList.toggle("selected")
+            arr[j+1].outerHTML = arr[j].outerHTML;
+            arr[j+1].classList.toggle("selected")
+              j--;
+          }
+          current.classList.toggle("pointer")
+          arr[j+1].outerHTML = current.outerHTML;
+      }
+      logg.innerHTML = 'Sorting: <span class="lTrue">complete</span>'
+      sortButton.disabled  = false
+      amountRange.disabled  = false
+      resetButton.disabled  = false
+}
